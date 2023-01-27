e2a62d0d80903935fb7d4b5159aeb5479959fe86
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2023-01-27 10:19:14 -0300

    Algumas coisas (teste)

--- Tools.hx
+++ Tools.hx
@@ -8,6 +8,11 @@ import openfl.utils.Assets;
 import haxe.CallStack;
 import flixel.FlxG;
 import openfl.Lib;
+#if android
+import android.Hardware;
+import android.Permissions;
+import android.os.Environment;
+#end
 #if desktop 
 import sys.io.File;
 import sys.FileSystem;
@@ -16,6 +21,39 @@ using StringTools;
 
 class Tools
 {
+	static var path:String = lime.system.System.applicationStorageDirectory;
+	public static var mode:Modes = ROOTDATA;
+	private static var path:String = null;
+	
+	// by Sirox
+	public static function returnPath(m:Modes = ROOTDATA):String {
+		#if android
+		if (m == ROOTDATA && mode != ROOTDATA) { // the most stupid checking i made
+			m = mode;
+		}
+		switch (m) {
+			case ROOTDATA:
+				path = lime.system.System.applicationStorageDirectory;
+			case INTERNAL:
+			    path = Environment.getExternalStorageDirectory() + '/' + '.' + Application.current.meta.get('file') + '/';
+				if (!FileSystem.exists(path)) {
+					FileSystem.createDirectory(path);
+				}
+			/*case ANDROIDDATA:
+			    path = Environment.getDataDirectory() + '/';*/
+		}
+		if (path != null && path.length > 0) {
+			trace(path);
+			return path;
+		}
+		trace('DEATH');
+		return null;
+		#else
+		path = '';
+		return path;
+		#end
+	}
+	
         // by Musk
         public static function exists(folder:String, ?type:AssetType = null):Bool
 	{
@@ -54,6 +92,16 @@ class Tools
 	{
 		      return Assets.getText(library);
 	}
+	
+	public static function getBytes(library:String):String
+	{
+		      return Assets.getBytes(library);
+	}
+	
+	public static function getBitmapData(library:String):String
+	{
+		      return Assets.getBitmapData(library);
+	
   
         public static function copyFolder(path:String, copyTo:String)
         {
@@ -72,6 +120,25 @@ class Tools
 		#end
 	}
   
+	public static function getPath(id:String, ?ext:String = "")
+	{
+		#if android
+		var file = Assets.getBytes(id);
+
+		var md5 = Md5.encode(Md5.make(file).toString());
+
+		if (FileSystem.exists(path + md5 + ext))
+			return path + md5 + ext;
+
+
+		File.saveBytes(path + md5 + ext, file);
+
+		return path + md5 + ext;
+		#else
+		return #if sys Sys.getCwd() + #end id;
+		#end
+	}
+	
         // by Musk
         public static function readDirectory(library:String):Array<String>
 	{
